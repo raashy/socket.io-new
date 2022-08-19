22e04431b8ba0393bd48ed59e1ecf9bb52c73470
raashy <hi@rashy.me>
2022-08-20 01:38:32 +0530

    make it better 2

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const io = require('socket.io')(server, {
 io.on("connection", (socket) => {
   console.log("New client connected");
   socket.on("disconnect", () => console.log("Client disconnected"));
-  socket.on("Text-change", (data) => {
-   
-    socket.broadcast.emit("change", data);
+
+  socket.on("text-change", (delta) => {
+    console.log(delta);
+    socket.broadcast.emit("update", delta);
   })
 });
